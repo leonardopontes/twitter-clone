c5ba7a607c2a3b59757e4da57c44f073c46b6178
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-09-25 12:58:56 -0300

    Update CommentItem.tsx

--- components/posts/CommentItem.tsx
+++ components/posts/CommentItem.tsx
@@ -4,25 +4,38 @@ import { formatDistanceToNowStrict } from 'date-fns';
 
 import Avatar from '../Avatar';
 
+// Definir uma interface de Propriedade de comentários Feed
 interface CommentItemProps {
+  // dados possuindo: Registro de <string, qualquer>;
   data: Record<string, any>;
 }
 
+// Item de comentário possuindo: React.FC<interface> ligando a ({ dados ligando a um Array vazio }) contendo...
 const CommentItem: React.FC<CommentItemProps> = ({ data = {} }) => {
+  // roteador ligando ao uso de Roteador
   const router = useRouter();
 
+  // ir para Usuário ligando ao uso de Callback com ((evento: qualquer) contendo...
   const goToUser = useCallback((ev: any) => {
+    // evento. de parar Propagação
     ev.stopPropagation();
 
+    // roteador. inserido na rota ((`/users/${data.user.id}`) com dados de usuário do id)
     router.push(`/users/${data.user.id}`)
+    // Envolver na estrutura roteador, dados de usuário do id
   }, [router, data.user.id]);
 
+  // criado Em ligado ao uso Memo, contendo...
   const createdAt = useMemo(() => {
+    // sendo verdade a negação de dados? e criado Em...
     if (!data?.createdAt) {
+      // retornar nulo
       return null;
     }
 
+    // retornar formatar Distancia Ate Agora Estrita com (nova Data com (dados. criado Em));
     return formatDistanceToNowStrict(new Date(data.createdAt));
+    // Envolver na estrutura dados. criado Em
   }, [data.createdAt])
 
   return (
