3e9ba19c07446c3268c485b51bfcc9453f56f43e
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-09-25 12:22:31 -0300

    Update CommentFeed.tsx

--- components/posts/CommentFeed.tsx
+++ components/posts/CommentFeed.tsx
@@ -1,13 +1,19 @@
 import CommentItem from './CommentItem';
 
+// Definir uma interface de Propriedade de comentários Feed
 interface CommentFeedProps {
+  // comentários?: contendo Registro de <string, qualquer> e um Array vazio;
   comments?: Record<string, any>[];
 }
 
+// comentários Feed possuindo: React.FC<interface> ligando a ({ comentários que liga a um array vazio }) contendo...
 const CommentFeed: React.FC<CommentFeedProps> = ({ comments = [] }) => {
+  // retorno
   return (
     <>
+      {/* {mapear.comentários com((comentário: possuindo Registro de <string, qualquer>,) contendo... */}
       {comments.map((comment: Record<string, any>,) => (
+        {/* chave ligando a {comentário. com base no id} e dados ligando a {comentário} */}
         <CommentItem key={comment.id} data={comment} />
       ))}
     </>
