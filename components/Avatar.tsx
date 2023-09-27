f055b4dab29df4e9595f5fea87d1b4a76158586d
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-09-27 09:32:06 -0300

    Update Avatar.tsx

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -4,23 +4,35 @@ import { useCallback } from "react";
 
 import useUser from "@/hooks/useUser";
 
+// Definir uma interface de Propriedade Avatar {
 interface AvatarProps {
+  // Id de usuário possuindo: string;
   userId: string;
+  // é Grande? possuindo: boolean;
   isLarge?: boolean;
+  // tem Borda? possuindo: boolean;
   hasBorder?: boolean;
 }
 
+// Avatar possuindo: React.FC<interface> ligando a = ({ Id de usuário, é Grande, tem Borda }) contendo... => {
 const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
+  // rota igualando ao = uso de Rota();
   const router = useRouter();
 
+  // { dados possuindo: Usuário buscado } igualando ao = uso de Usuário(com Id de usuário);
   const { data: fetchedUser } = useUser(userId);
 
+  // Click ligado ligando ao = uso de Callback com ((evento: qualquer) contendo... => {
   const onClick = useCallback((event: any) => {
+    // evento. de parar Propagação();
     event.stopPropagation();
 
+    // url ligando a = na rota `/users/${userId}`; (Id de usuário)
     const url = `/users/${userId}`;
 
+    // roteador. inserido na (url);
     router.push(url);
+    // }, Envolver na estrutura [rota, Id de usuário]);
   }, [router, userId]);
 
   return (
