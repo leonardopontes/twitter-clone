5f48808cf0575584d7ba49e63d0d884f352c0d03
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-09-27 09:59:48 -0300

    Update Form.tsx

--- components/Form.tsx
+++ components/Form.tsx
@@ -11,40 +11,65 @@ import usePost from '@/hooks/usePost';
 import Avatar from './Avatar';
 import Button from './Button';
 
+// Definir uma interface de Propriedade de Formulário {
 interface FormProps {
+  // espaço reservado possuindo: string;
   placeholder: string;
+  // é Comentário? possuindo: boolean;
   isComment?: boolean;
+  // Id de Post? possuindo: string;
   postId?: string;
 }
 
+// Formulário possuindo: React.FC<interface> ligando a = ({ espaço reservado, é Comentário, Id de post }) contendo... => {
 const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
+  // Modal de registro igualando ao = uso de Modal de Registro();
   const registerModal = useRegisterModal();
+  // Modal de login igualando ao = uso de Modal de Login();
   const loginModal = useLoginModal();
 
+  // { dados possuindo: Usuário atual } igualando ao = uso de Usuário Atual();
   const { data: currentUser } = useCurrentUser();
+  // { mutação possuindo: mutação de Posts } igualando ao = uso de Posts();
   const { mutate: mutatePosts } = usePosts();
+  // { mutação possuindo: mutação de Post } igualando ao = uso de Post(com Id do Post como string);
   const { mutate: mutatePost } = usePost(postId as string);
 
+  // [corpo, definir Corpo] ligado ao = uso de Estado('') vazio;
   const [body, setBody] = useState('');
+  // [está Carregando, definir se Está Carregando] ligado ao = uso de Estado(falso);
   const [isLoading, setIsLoading] = useState(false);
 
+  // Envio ativado, ligando ao = uso de Callback(assíncrono () contendo... => {
   const onSubmit = useCallback(async () => {
+    // Exceção de tentar {, definir se Está Carregando de forma (verdadeira)
     try {
       setIsLoading(true);
 
+      // url ligando a = é Comentário ? na rota `/api/comments?postId=${postId}`; (Id de Post) e na rota : '/api/posts';
       const url = isComment ? `/api/comments?postId=${postId}` : '/api/posts';
 
+      // aguardar axios.cria na (url, { corpo });
       await axios.post(url, { body });
 
+      // toast.sucesso('Tweet criado');
       toast.success('Tweet created');
+      // definirCorpo('') vazio;
       setBody('');
+      // mutação de Posts();
       mutatePosts();
+      // mutação de Post();
       mutatePost();
+    // } pegar (erro) { 
     } catch (error) {
+      // toast.erro('Algo deu errado');
       toast.error('Something went wrong');
+    // } finalizar {  
     } finally {
+      // definir se Está Carregando(falso);
       setIsLoading(false);
     }
+    // }, Envolver na estrutura [corpo, mutação de Posts, é Comentário, Id de post, mutação de Post]);
   }, [body, mutatePosts, isComment, postId, mutatePost]);
 
   return (
