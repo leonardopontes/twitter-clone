178bbf0192270230b1e6c71ecd14afde3535685c
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-09-28 10:32:51 -0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,16 +2,24 @@ import { useRouter } from "next/router";
 import { useCallback } from "react";
 import { BiArrowBack } from "react-icons/bi";
 
+// Definir uma interface de Propriedade de Header {
 interface HeaderProps {
+  // mostrar Seta para Trás? possuindo: boolean;
   showBackArrow?: boolean;
+  // rótulo como: string
   label: string;
 }
 
+// Header possuindo: React.FC<interface> ligando a = ({ mostrar Seta para Trás, rótulo }) contendo... => {
 const Header: React.FC<HeaderProps> = ({showBackArrow, label }) => {
+  // rota ligando a = uso de Rota();
   const router = useRouter();
 
+  // Lidar com volta ligando a = uso de Callback(() contendo... => {
   const handleBack = useCallback(() => {
+    // voltar.rota();
     router.back();
+    // }, Envolver na estrutura a [rota]);
   }, [router]);
 
   return (
