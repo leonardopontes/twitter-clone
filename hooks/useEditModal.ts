c99df42feceda33f91cc10e967aa01afb9454da1
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-09-29 10:18:03 -0300

    Update useEditModal.ts

--- hooks/useEditModal.ts
+++ hooks/useEditModal.ts
@@ -1,14 +1,22 @@
 import { create } from 'zustand';
 
+// Criar uma interface de Editar Modal de Loja {
 interface EditModalStore {
+  // Abrir como: booleano;
   isOpen: boolean;
+  // Abrir ativado como: () => vazio;
   onOpen: () => void;
+  // Fechar ativado como: () => vazio;
   onClose: () => void;
 }
 
+// uso de Editar Modal ligado a = criar<Interface>((definir) contendo... => ({
 const useEditModal = create<EditModalStore>((set) => ({
+  // Abrir como: falso,
   isOpen: false,
+  // Abrir ativado como: () => definir({ Abrir como: verdadeiro }),
   onOpen: () => set({ isOpen: true }),
+  // Fechar ativado como: () => definir({ Abrir como: falso })
   onClose: () => set({ isOpen: false })
 }));
 
