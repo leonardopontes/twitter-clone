f2ccf02292238518ad2d6559892a0924b72c3558
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-09-30 16:10:06 -0300

    Update usePosts.ts

--- hooks/usePosts.ts
+++ hooks/usePosts.ts
@@ -2,8 +2,11 @@ import useSWR from 'swr';
 
 import fetcher from '@/libs/fetcher';
 
+// uso de Posts ligado a = (Id de usuário?: string) contendo... => {
 const usePosts = (userId?: string) => {
+  // url ligado a = Id de usuário ? na rota `/api/posts?userId=${userId}` : '/api/posts';
   const url = userId ? `/api/posts?userId=${userId}` : '/api/posts';
+  // { dados, erro, está Carregando, mutação } ligado a = uso de SWR(url, buscador);
   const { data, error, isLoading, mutate } = useSWR(url, fetcher);
 
   return {
