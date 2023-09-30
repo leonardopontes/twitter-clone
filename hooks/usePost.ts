0d7d5a5c8591cba99f5e25218e7961926b3f66ee
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-09-30 16:04:58 -0300

    Update usePost.ts

--- hooks/usePost.ts
+++ hooks/usePost.ts
@@ -2,7 +2,9 @@ import useSWR from 'swr';
 
 import fetcher from '@/libs/fetcher';
 
+// uso de Post ligado a = (Id de post?: string) contendo... => {
 const usePost = (postId: string) => {
+  // { dados, erro, está Carregando, mutação } ligado a = uso de SWR(Id de post ? na rota `/api/posts/${postId}` : nulo, buscador);
   const { data, error, isLoading, mutate } = useSWR(postId ? `/api/posts/${postId}` : null, fetcher);
 
   return {
