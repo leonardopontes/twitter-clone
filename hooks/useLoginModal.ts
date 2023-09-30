a0016e31d6dbfce1353d6e1d64ba3d4b468214d8
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-09-30 15:44:58 -0300

    Update useLoginModal.ts

--- hooks/useLoginModal.ts
+++ hooks/useLoginModal.ts
@@ -1,14 +1,22 @@
 import { create } from 'zustand';
 
+// Criar uma interface de Login Modal de Loja {
 interface LoginModalStore {
+  // Abrir como: booleano;
   isOpen: boolean;
+  // Abrir ativado como: () => vazio;
   onOpen: () => void;
+  // Fechar ativado como: () => vazio;
   onClose: () => void;
 }
 
+// uso de Login Modal ligado a = criar<Interface>((definir) contendo... => ({
 const useLoginModal = create<LoginModalStore>((set) => ({
+  // Abrir como: falso,
   isOpen: false,
+  // Abrir ativado como: () => definir({ Abrir como: verdadeiro }),
   onOpen: () => set({ isOpen: true }),
+  // Fechar ativado como: () => definir({ Abrir como: falso })
   onClose: () => set({ isOpen: false })
 }));
 
