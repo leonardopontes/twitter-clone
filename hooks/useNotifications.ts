572ccb20a18d350cd4a28c59b9346b6f37c0431e
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-09-30 15:56:25 -0300

    Update useNotifications.ts

--- hooks/useNotifications.ts
+++ hooks/useNotifications.ts
@@ -2,8 +2,11 @@ import useSWR from 'swr';
 
 import fetcher from '@/libs/fetcher';
 
+// uso de Notificações ligado a = (Id de usuário?: string) contendo... => {
 const useNotifications = (userId?: string) => {
+  // url ligado a = Id de usuário ? na rota `/api/notifications/${userId}` : nulo;
   const url = userId ? `/api/notifications/${userId}` : null;
+  // { dados, erro, está Carregando, mutação } ligado a = uso de SWR(url, buscador);
   const { data, error, isLoading, mutate } = useSWR(url, fetcher);
 
   return {
