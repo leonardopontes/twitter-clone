4f9da7f2cc830683bae24d048c7fd041cc0ff288
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-10-01 11:18:14 -0300

    Update prismadb.ts

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -1,10 +1,14 @@
 import { PrismaClient } from "@prisma/client"
 
+// declarar global {
 declare global {
+  // prisma: Client Prisma | indefinido
   var prisma: PrismaClient | undefined
 }
 
+// client ligando a = global Então.prisma || novo Client Prisma()
 const client = globalThis.prisma || new PrismaClient()
+// sendo verdade o (processo.de envio.ENVIO_NODE !== "produção) global Então.prisma ligando a = client
 if (process.env.NODE_ENV !== "production") globalThis.prisma = client
 
 export default client
