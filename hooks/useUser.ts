d1a0df358bc5e1dd0b31bd963a2481951eed46b6
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-10-01 11:05:40 -0300

    Update useUser.ts

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -2,7 +2,9 @@ import useSWR from 'swr';
 
 import fetcher from '@/libs/fetcher';
 
+// uso de Usuário ligado a = (Id de usuário: string) contendo... => {
 const useUser = (userId: string) => {
+  // { dados, erro, está Carregando, mutação } ligado a = uso de SWR(Id de usuário ? na rota `/api/users/${userId}` : nulo, buscando);
   const { data, error, isLoading, mutate } = useSWR(userId ? `/api/users/${userId}` : null, fetcher);
 
   return {
