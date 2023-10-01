ed9381bbc8ffb35f280407743ba74b88b2906e18
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-10-01 11:13:42 -0300

    Update fetcher.ts

--- libs/fetcher.ts
+++ libs/fetcher.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// buscando ligado a = (url como: string) contendo => axios.pegar(url).então((responder) com => responder. com dados);
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 export default fetcher;
