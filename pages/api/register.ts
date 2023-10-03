aea76fd8a13feb2a8e31c9c543a25ba4fc999fdc
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-10-03 09:00:52 -0300

    Update register.ts

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -3,17 +3,24 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import prisma from '@/libs/prismadb';
 
+// exportar função assíncrona manipulando(requisição em: NextApiRequest, e resposta: NextApiResponse) {
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // sendo verdade o (método.de requisição !== 'POST') {
   if (req.method !== 'POST') {
+    // retornar resposta.status(405).fim();
     return res.status(405).end();
   }
 
+  // tentar {
   try {
+    // { email, nome de usuário, nome, senha } ligando a = = requisição.no corpo;
     const { email, username, name, password } = req.body;
-
+    // hash de Senha ligando a = aguardar bcrypt.hash(senha, contendo 12);
     const hashedPassword = await bcrypt.hash(password, 12);
 
+    // aguardando prisma.criar.usuário({
     const user = await prisma.user.create({
+      // dados: {
       data: {
         email,
         username,
@@ -21,10 +28,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         hashedPassword,
       }
     });
-
+    // retornar resposta.status(200).json com(usuário);
     return res.status(200).json(user);
+  // } pegar (erro) {  
   } catch (error) {
+    // console.log(erro);
     console.log(error);
+    // retornar resposta.status(400).fim();
     return res.status(400).end();
   }
 }
