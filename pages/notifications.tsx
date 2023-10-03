39e7d9e96ae8764d2701c88043cd1b2e5be04b8c
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-10-03 16:25:42 -0300

    Update notifications.tsx

--- pages/notifications.tsx
+++ pages/notifications.tsx
@@ -4,20 +4,29 @@ import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 
+// exportar função de pegar Propriedades do Lado Servidor no(contexto: Contexto de Página Next) {
 export async function getServerSideProps(context: NextPageContext) {
+  // sessão ligando a = aguardar pegar Sessão(contexto);
   const session = await getSession(context);
 
+  // se (negação de sessão) for verdade {
   if (!session) {
+    // retornar {
     return {
+      // redirecionar: {
       redirect: {
+        // destinar: raíz da rota,
         destination: '/',
+        // permanente: falso,
         permanent: false,
       }
     }
   }
-
+  // retornar {
   return {
+    // propriedade: {
     props: {
+      // sessão
       session
     }
   }
